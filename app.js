6cad1143ffdb5ef6329de58f7b87a89717f5427a
Dallas Phelps <dallasphelps@ymail.com>
2021-09-05 12:25:34 -0500

    Use MVC pattern to manage application logic and added hbs partials

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
+var roomsRouter = require('./app_server/routes/rooms');
+var newsRouter = require('./app_server/routes/news');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
+
+// register handlebars partials(https://www.npmjs.com/package/hbs)
+hbs.registerPartials(path.join(__dirname, 'app_server','views/partials'));
+
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
@@ -21,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/travel', travelRouter);
+app.use('/rooms', roomsRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
