3c3f51ac86d5c017795c400c80eadc802b36fdfd
Dallas Phelps <dallasphelps@ymail.com>
2021-09-10 22:20:30 -0500

    Fixed error that caused travel page not to load and server crash

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,7 +3,7 @@ const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Travel';
-    res.render('travel',  {title: pageTitle}, trips);
+    res.render('travel',  {title: pageTitle, trips});
 };
 
 module.exports = {
