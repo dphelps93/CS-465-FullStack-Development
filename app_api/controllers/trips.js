bf7b28ac9206eb4c1c6d5f25021043550e544dee
Dallas Phelps <dallasphelps@ymail.com>
2021-10-01 00:44:43 -0500

    Created and added SPA page to manage trip data on Traveler Getaways

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,3 +1,4 @@
+const { model } = require('mongoose');
 const mongoose = require('mongoose');
 const Model = mongoose.model('trips');
 
@@ -9,7 +10,7 @@ const tripsList = async (req, res) => {
             if (!trips) {
                 return res
                     .status(404)
-                    .json({"message": "trip not found"});
+                    .json({ "message": "trip not found" });
             } else if (err) {
                 return res
                     .status(404)
@@ -25,12 +26,12 @@ const tripsList = async (req, res) => {
 //GET: /trips/:tripCode finds a trip by code
 const tripsFindCode = async (req, res) => {
     Model
-        .find({'code': req.params.tripCode}) //empty filter for all
+        .find({ 'code': req.params.tripCode }) //empty filter for all
         .exec((err, trips) => {
             if (!trips) {
                 return res
                     .status(404)
-                    .json({"message": "trip not found"});
+                    .json({ "message": "trip not found" });
             } else if (err) {
                 return res
                     .status(404)
@@ -43,7 +44,102 @@ const tripsFindCode = async (req, res) => {
         });
 };
 
+const tripsAddTrip = async (req, res) => {
+    Model
+        .create({
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        },
+            (err, trip) => {
+                if (err) {
+                    return res
+                        .status(400) //bad request
+                        .json(err);
+                } else {
+                    return res
+                        .status(201) // successfully created
+                        .json(trip);
+                }
+            });
+}
+
+const tripsUpdateTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+        .findOneAndUpdate({ 'code': req.params.tripCode }, {
+            code: req.body.code,
+            name: req.body.name,
+            length: req.body.length,
+            start: req.body.start,
+            resort: req.body.resort,
+            perPerson: req.body.perPerson,
+            image: req.body.image,
+            description: req.body.description
+        }, { new: true })
+        .then(trip => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+}
+
+const tripsDeleteTrip = async (req, res) => {
+    console.log(req.body);
+    Model
+        .findOneAndDelete({'code': req.params.tripCode})
+        .then(trip => {
+            if (!trip) {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            res.send(trip);
+        }).catch(err => {
+            if (err.kind === 'ObjectId') {
+                return res
+                    .status(404)
+                    .send({
+                        message: "Trip not found with code "
+                            + req.params.tripCode
+                    });
+            }
+            return res
+                .status(500) // server error
+                .json(err);
+        });
+}
+
 module.exports = {
     tripsList,
-    tripsFindCode
+    tripsFindCode,
+    tripsAddTrip,
+    tripsUpdateTrip,
+    tripsDeleteTrip
 };
